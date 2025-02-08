c7711a98e7a0e7cabb40ff2a096d330efac40be2
kingkyle <38464469+kingkyle@users.noreply.github.com>
2025-02-08 14:50:02 +0100

    feat(web): enhance Hero and Services components with improved layout and styling

--- apps/web/src/app/services/in-home-care/_components/hero.tsx
+++ apps/web/src/app/services/in-home-care/_components/hero.tsx
@@ -6,20 +6,33 @@ import React from "react";
 export default function Hero() {
   return (
     <div className='bg-white bg-[url("/images/bg-pattern-1.png")] bg-no-repeat py-[9.375rem]'>
-      <div className="container flex">
-        <div className="space-y-[24px] w-full md:w-1/2">
+      <div className="container grid gap-8 items-center lg:grid-cols-2 overflow-hidden">
+        <div className="space-y-[24px]">
           <CBadge>In-home care</CBadge>
-          <h1 className="font-clash text-[4.875rem] font-medium text-black leading-[5.938rem]">
+          <h1 className="font-clash text-[2rem] md:text-[3.5rem] lg:text-[4.875rem] font-medium text-black leading-[2.438rem] md:leading-[3.638rem] lg:leading-[5.938rem]">
             Comfort and Care,<span className="block text-[#B6965A]">Right at Home</span>
           </h1>
-          <p className="text-[#475467] max-w-3xl text-xl">Personalized in-home support for seniors to live safely and independently in familiar surroundings</p>
+          <p className="text-[#475467] max-w-3xl text-xl">
+            Personalized in-home support for seniors to live safely and independently in familiar
+            surroundings
+          </p>
           <div>
-            <Button size="lg" className="min-h-[3.75rem] bg-[#7A8D83] border-2 border-[#FFFFFF1F] hover:bg-[#7A8D83s] rounded-[29px] shadow-md shadow-[#1018280D] text-lg font-bold">Submit a request</Button>
+            <Button
+              size="lg"
+              className="min-h-[3.75rem] bg-[#7A8D83] border-2 border-[#FFFFFF1F] hover:bg-[#7A8D83s] rounded-[29px] shadow-md shadow-[#1018280D] text-lg font-bold"
+            >
+              Submit a request
+            </Button>
           </div>
         </div>
-        <div className="w-full md:w-1/2 ">
-          <div className="relative max-w-[657px] mx-auto">
-          <Image src="/images/hero-1.png" width={657} height={548} className="object-contain relative w-full rounded-[60px]" alt="in-home care"/>
+        <div className="mt-[3rem] md:mt-0">
+          <div className="w-full h-full mx-auto relative">
+            <Image
+              src="/images/hero-1.png"
+              width={657} height={548}
+              className="object-cover rounded-[60px] mx-auto "
+              alt="in-home care"
+            />
           </div>
         </div>
       </div>
--- apps/web/src/app/services/in-home-care/_components/services.tsx
+++ apps/web/src/app/services/in-home-care/_components/services.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import CBadge from "@/components/CBadge";
+import React from "react";
 
 export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <div>
+      <div className="space-y-[2rem] container text-center py-[6rem]">
+        <CBadge>In-home Services</CBadge>
+        <h3 className="text-[2rem] md:text-[5rem] font-clash font-medium text-black leading-[2.438rem] md:leading-[5.063rem]">
+          What You Can Get Through our <span className="md:block text-[#7A8D83]">In-Home Care Service</span>
+        </h3>
+        <p className="max-w-screen-lg mx-auto text-[#475467] text-xl">
+          From basic non-medical support like help with bathing, dressing, meal preparation, and
+          housekeeping, to more advanced services such as wound care, medication management,
+          physical therapy and more.
+        </p>
+      </div>
+    </div>
+  );
 }
